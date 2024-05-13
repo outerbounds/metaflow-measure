d5e29d841709fa0cb681933dfeaf67db444eb2e6
Ville Tuulos <tuulos@gmail.com>
2024-05-12 23:08:23 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 from setuptools import setup, find_namespace_packages
 
-version = "0.0.1"
+version = "0.0.2"
 
 setup(
     name="metaflow-measure",
     version=version,
-    description="Measure metrics and timing in Metaflow steps, send them optionally to Datadog or other backends."
+    description="Measure metrics and timings in Metaflow steps and send them optionally to Datadog or other backends",
     author="Outerbounds",
     author_email="hello@outerbounds.co",
     packages=find_namespace_packages(include=["metaflow_extensions.*"]),
     py_modules=[
         "metaflow_extensions",
-    ],
-    install_requires=[
-         "metaflow"
     ]
 )
